1173739829f4cf1451c80575a161c2de09c3d6fd
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-16 10:31:42 -0600

    created basic console for user to type in number and store as variable

---
+++ backendGuessingGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace backendGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Guess a secret number: ");
+            string guess = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine($"You guessed: {guess}");
+        }
+    }
+}

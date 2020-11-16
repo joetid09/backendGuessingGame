6e148f7af30f7dabeb1b93e7c688051ef08654aa
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-16 12:03:16 -0600

    cheat mode activate, allows user to guess infinitely until secret number is guessed

--- backendGuessingGame/Program.cs
+++ backendGuessingGame/Program.cs
@@ -25,6 +25,10 @@ namespace backendGuessingGame
             {
                 hard(secretNumber);
             }
+            else if (mode == "cheats")
+            {
+                cheats(secretNumber);
+            }
         }
         static void easy(int secretNumber)
         {
@@ -89,6 +93,36 @@ namespace backendGuessingGame
         static void hard(int secretNumber)
         {
             for (int i = 1; i < 5; i++)
+            {
+                Console.Write("Guess a secret number: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("Congratulations, you have guessed correctly!");
+                    break;
+                }
+                else if (i < 4)
+                {
+                    if (guess > secretNumber)
+                    {
+                        Console.WriteLine($"Your guess {guess} was too high. You have {4 - i} tries left,  try again?");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your guess {guess} was too low. You have {4 - i} tries left,  try again?");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"4 guesses and still wrong?? I'm cutting you off, get outta HERE!");
+                }
+            }
+        }
+        static void cheats(int secretNumber)
+        {
+            for (int i = 1; i >= 0; i++)
             {
                 Console.Write("Guess a secret number: ");
                 int guess = Convert.ToInt32(Console.ReadLine());

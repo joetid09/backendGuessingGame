8c4c2aa09387af1732600209bf13113216370416
joseph tidwell <joseph.tidwell09@gmail.com>
2020-11-16 11:47:22 -0600

    created easy, medium, and hard game modes. provided choice input at beginning for difficulty

--- backendGuessingGame/Program.cs
+++ backendGuessingGame/Program.cs
@@ -9,6 +9,85 @@ namespace backendGuessingGame
             int secretNumber = new Random().Next(1, 100);
             Console.WriteLine($" secret number: {secretNumber}");
 
+            Console.WriteLine("What mode would you like to play? Easy, Medium, or Hard");
+            string choiceInput = Console.ReadLine();
+            string mode = choiceInput.ToLower();
+
+            if (mode == "easy")
+            {
+                easy(secretNumber);
+            }
+            else if (mode == "medium")
+            {
+                medium(secretNumber);
+            }
+            else if (mode == "hard")
+            {
+                hard(secretNumber);
+            }
+        }
+        static void easy(int secretNumber)
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Console.Write("Guess a secret number: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("Congratulations, you have guessed correctly!");
+                    break;
+                }
+                else if (i < 8)
+                {
+                    if (guess > secretNumber)
+                    {
+                        Console.WriteLine($"Your guess {guess} was too high. You have {8 - i} tries left,  try again?");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your guess {guess} was too low. You have {8 - i} tries left,  try again?");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"8 guesses and still wrong?? I'm cutting you off, get outta HERE!");
+                }
+            }
+        }
+        static void medium(int secretNumber)
+        {
+            for (int i = 1; i < 7; i++)
+            {
+                Console.Write("Guess a secret number: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("Congratulations, you have guessed correctly!");
+                    break;
+                }
+                else if (i < 6)
+                {
+                    if (guess > secretNumber)
+                    {
+                        Console.WriteLine($"Your guess {guess} was too high. You have {6 - i} tries left,  try again?");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Your guess {guess} was too low. You have {6 - i} tries left,  try again?");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"6 guesses and still wrong?? I'm cutting you off, get outta HERE!");
+                }
+            }
+        }
+        static void hard(int secretNumber)
+        {
             for (int i = 1; i < 5; i++)
             {
                 Console.Write("Guess a secret number: ");
